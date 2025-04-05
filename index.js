c3fd4cd4441751f026fd6d73801a54c033ebe1cd
Naman Aryan <160505477+NamanAryan@users.noreply.github.com>
2025-04-05 20:30:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const data = require('./data.json'); 
+const data = require('./data.json');
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('API is running. Visit /api/data to see the JSON.');
+});
+
+// JSON API route
 app.get('/api/data', (req, res) => {
   res.json(data);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
